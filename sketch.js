89c1e400f734158a0f5699ea5280985a1e83326a
Va1shnav1 <75217694+Va1shnav1@users.noreply.github.com>
2021-01-24 11:00:14 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,107 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var boxBase;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 200, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color("gray");
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.2, friction:1.25, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+ 	boxPosition=width/2-100
+ 	boxY=610;
+
+
+ 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
+
+
+	Engine.run(engine);
+
+	
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+ 
+  keyPressed();
+
+  packageSprite.x= helicopterSprite.x;
+  if(packageSprite.isTouching(boxBase)){
+	  background("green");
+	  fill(0, 79, 41);
+	  strokeWeight(3);
+	  stroke(0);
+	  textSize(35);
+	  text("TASK SUCCESSFUL",250, 300);
+  }
+  
+  drawSprites();
+  
+  
+ 
+}
+
+function keyPressed() {
+	if(keyCode===RIGHT_ARROW){
+		helicopterSprite.velocityX=2;
+	}
+	if(keyCode===LEFT_ARROW){
+		helicopterSprite.velocityX=-2;
+	}
+	if(keyCode===DOWN_ARROW){
+		packageSprite.x= packageBody.position.x 
+ 		packageSprite.y= packageBody.position.y 
+
+		Matter.Body.setStatic(packageBody, false);
+	}
+}
